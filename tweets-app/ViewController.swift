f8d889fddd72d28eadd9a450f2c87b734c08c680
Jonathan Rivera <misael@MacBook-Air-de-Misael-5.local>
2020-04-09 13:12:15 -0500

    base project

---
+++ tweets-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tweets-app
+//
+//  Created by Misael Rivera on 09/04/20.
+//  Copyright © 2020 Misael Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
